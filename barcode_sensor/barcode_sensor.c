422408c713a89277a522cbc7bc63327742e1f21e
Benjamin Loh <xrando20@gmail.com>
2023-10-21 15:11:45 +0800

    Update barcode_sensor.c
    update to barcode module

--- barcode_sensor/barcode_sensor.c
+++ barcode_sensor/barcode_sensor.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include <math.h>
+# include <string.h>
+#include <stdlib.h>
 
-#define BARCODE_SENSOR_PIN 0
+#define BARCODE_SENSOR_PIN  0
 #define DEBOUNCE_TIME_USEC  100000
-#define ARRAY_SIZE 9
+#define ARRAY_SIZE          27  // used to store 1 barcode value denoted by *A* for example
+#define BARCODE_SENSE_TIME_MS  50 // time interval to sense barcode in ms
 
-bool debounce               (uint gpio, uint32_t events);
-void barcode_sensor_isr     (uint gpio, uint32_t events);
-void ir_sensor_init         (uint ir_sensor_pin, void * ir_sensor_isr);
-void print_barcode          (volatile int barcode[ARRAY_SIZE]);
-int  find_smallest_value    (volatile int barcode[ARRAY_SIZE]);
-void scale_down_barcode     (volatile int barcode[ARRAY_SIZE]);
+bool    debounce               (uint gpio, uint32_t events);
+void    barcode_sensor_isr     (uint gpio, uint32_t events);
+void    ir_sensor_init         (uint ir_sensor_pin, void * ir_sensor_isr);
+void    print_barcode          (char * barcode_string);
+int     find_smallest_value    (volatile int barcode[ARRAY_SIZE]);
+char *  scale_down_barcode     (volatile int barcode[ARRAY_SIZE]);
 
 
-volatile bool barcode_detected = false;
-volatile int barcode[ARRAY_SIZE] = {0};
-volatile int index = 0;
+volatile bool   barcode_detected            = false;
+volatile int    barcode[ARRAY_SIZE]         = {0};
+volatile int    g_index                       = 0;
 
 // barcode values 2d array, first column is ascii value,
 // second column is barcode value
-volatile char * barcode_values_array[44][2] = 
+// store this in header file
+const char * barcode_values_array[44][2] = 
 {
     {"111221211","0"},
     {"211211112","1"},
@@ -68,7 +72,7 @@ volatile char * barcode_values_array[44][2] =
     {"121121211","*"}
 };
 
-// Function to debounce button
+// Function to debounce button (use debounce from line_sensor.c)
 bool debounce (uint gpio, uint32_t events) 
 {
     // Get start time
@@ -95,13 +99,14 @@ bool debounce (uint gpio, uint32_t events)
 bool
 repeating_timer_callback_isr (struct repeating_timer *p_timer) 
 {
-    // Print time counter
-    //printf("Time: %us\n", (*(uint *) p_timer->user_data)++);
+    // Increment counter
     (*(uint *) p_timer->user_data)++;
     return true;
 }
 
- void barcode_sensor_isr (uint gpio, uint32_t events) 
+// to integrate with isr in line sensor module
+void
+barcode_sensor_isr (uint gpio, uint32_t events) 
 {
     static uint                    time_counter = 0u;
     static struct repeating_timer  timer        = {0};
@@ -120,16 +125,13 @@ repeating_timer_callback_isr (struct repeating_timer *p_timer)
                 // stop counting time
                 cancel_repeating_timer(&timer);
                 // store in array
-                barcode[index] = time_counter;
-                index++;
+                barcode[g_index] = time_counter;
+                g_index++;
                 time_counter = 0;
             }
             // count time for black
-            add_repeating_timer_ms(-50, repeating_timer_callback_isr, 
+            add_repeating_timer_ms(-BARCODE_SENSE_TIME_MS, repeating_timer_callback_isr, 
                                 &time_counter, &timer);
-            
-            
-            
             barcode_detected = true;
         }
         else
@@ -142,15 +144,13 @@ repeating_timer_callback_isr (struct repeating_timer *p_timer)
                 cancel_repeating_timer(&timer);
                 //printf("Final Time: %us\n", time_counter);
                 // store in array
-                barcode[index] = time_counter;
-                index++;
+                barcode[g_index] = time_counter;
+                g_index++;
                 time_counter = 0;
             }
             // count time for white
-            add_repeating_timer_ms(-50, repeating_timer_callback_isr, 
+            add_repeating_timer_ms(-BARCODE_SENSE_TIME_MS, repeating_timer_callback_isr, 
                               &time_counter, &timer);
-            
-            
             barcode_detected = false;
         }
     }
@@ -158,8 +158,9 @@ repeating_timer_callback_isr (struct repeating_timer *p_timer)
 }
 
 
-// init ir sensor on gpio pin
-void ir_sensor_init (uint ir_sensor_pin, void * ir_sensor_isr) 
+// init ir sensor on gpio pin (replace with function from line_sensor.c)
+void
+ir_sensor_init (uint ir_sensor_pin, void * ir_sensor_isr) 
 {
     gpio_init(ir_sensor_pin);
     gpio_set_dir(ir_sensor_pin, GPIO_IN);
@@ -168,18 +169,20 @@ void ir_sensor_init (uint ir_sensor_pin, void * ir_sensor_isr)
                                         true, ir_sensor_isr);
 }
 
-// print barcode
-void print_barcode (volatile int barcode[ARRAY_SIZE])
-{
-    for (int i = 0; i < ARRAY_SIZE; i++)
-    {
-        printf("[%d]", barcode[i]);
-    }
-    printf("\n");
-}
+// print barcode (for debugging)
+// void
+// print_barcode (char * barcode_string)
+// {
+//     for (int i = 0; i < ARRAY_SIZE; i++)
+//     {
+//         printf("%d", barcode[i]);
+//     }
+//     printf("\n");
+// }
 
-// function to find smallest value in barcode array
-int find_smallest_value (volatile int barcode[ARRAY_SIZE])
+// function to find smallest value in barcode array (needs optimization)
+int
+find_smallest_value (volatile int barcode[ARRAY_SIZE])
 {
     int smallest_value = barcode[0];
     for (int i = 1; i < ARRAY_SIZE; i++)
@@ -195,56 +198,134 @@ int find_smallest_value (volatile int barcode[ARRAY_SIZE])
     return smallest_value;
 }
 
-// function to scale down values in barcode array
-void scale_down_barcode (volatile int barcode[ARRAY_SIZE])
+// function to scale down values in barcode array & return string representation
+// (needs optimization)
+// char *
+// scale_down_barcode (volatile int barcode[ARRAY_SIZE])
+// {
+//     // init string var
+//     char * barcode_string = "";
+//     int smallest_value = find_smallest_value(barcode);
+//     for (int i = 0; i < ARRAY_SIZE; i++)
+//     {
+//         if (barcode[i] != 0)
+//         {
+//             barcode[i] = floor(barcode[i] / smallest_value);
+//             if (barcode[i] > 1)
+//             {
+//                 barcode[i] = 2;
+//             }
+//             barcode_string += barcode[i];
+//         }
+//     }
+//     // add null terminator
+//     barcode_string += '\0';
+    
+//     return barcode_string;
+// }
+
+// function to scale down values in barcode array & return string representation
+char *
+scale_down_barcode (volatile int barcode[ARRAY_SIZE]) 
 {
-    int smallest_value = find_smallest_value(barcode);
-    for (int i = 0; i < ARRAY_SIZE; i++)
+    int  smallest_value  = find_smallest_value(barcode);
+    char *barcode_string_ascii = (char *)malloc(ARRAY_SIZE + 1); // +1 for the null terminator
+    int  string_index    = 0;
+
+    for (int i = 0; i < ARRAY_SIZE; i++) 
     {
-        if (barcode[i] != 0)
+
+        if (barcode[i] != 0) 
         {
+
             barcode[i] = floor(barcode[i] / smallest_value);
-            if (barcode[i] > 1)
+            if (barcode[i] > 1) 
             {
+
                 barcode[i] = 2;
+
             }
+            
+            // Only store ascii of barcode value, ignore *
+            if (i >= 9 && i < 18) 
+            {
+                
+                // Convert the integer to a character and store it in the string
+                barcode_string_ascii[string_index++] = '0' + barcode[i];
+
+            }
+
         }
+
     }
+
+    barcode_string_ascii[string_index] = '\0';
+
+    return barcode_string_ascii;
 }
 
 
-int main ()
+// convert barcode ascii to barcode value
+char *
+convert_barcode (char * barcode_string)
 {
-    stdio_init_all();
-    ir_sensor_init(BARCODE_SENSOR_PIN, &barcode_sensor_isr);
-
-    
-
-    for (;;)
+    char * barcode_value = "";
+    // compare barcode to barcode values array
+    for (int i = 0; i < 44; i++)
     {
-        // print state of barcode sensor
-        
-        //print barcode
-        //print_barcode(barcode);
-        //sleep_ms(1000);
 
-        // if barcode is full
-        if (index == ARRAY_SIZE)
+        // check if barcode string matches barcode ascii
+        if (strcmp(barcode_string, barcode_values_array[i][0]) == 0)
         {
-            // scale down barcode
-            scale_down_barcode(barcode);
-            //print barcode
-            print_barcode(barcode);
-            // reset index
-            index = 0;
+
+            barcode_value = barcode_values_array[i][1];
+
         }
 
     }
 
+    return barcode_value;
+}
 
-    
+// function to correct barcode values
+char *
+correct_barcode (char * barcode_string)
+{
+    // TODO
+    return barcode_string;
+}
 
+int main ()
+{
+    // init ir sensor
+    stdio_init_all();
+    ir_sensor_init(BARCODE_SENSOR_PIN, &barcode_sensor_isr);
 
+    for (;;)
+    {
+
+        // if all barcode ascii is collected
+        if (g_index == ARRAY_SIZE)
+        {
+
+            // scale down barcode & store in string
+            char * barcode_string = scale_down_barcode(barcode);
+            //print barcode
+            //print_barcode(barcode_string);
+            printf("%s\n", barcode_string);
+
+            // convert barcode ascii to barcode value
+            char * barcode_value = convert_barcode(barcode_string);
+            // print barcode value
+            printf("%s\n", barcode_value);
+            
+
+            // reset g_index
+            g_index = 0;
+
+        }
+
+    }
 
     return 0;
 }

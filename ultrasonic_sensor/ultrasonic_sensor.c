9be5e675ed5b41252a7bf243f3988246249c6660
seankzw <56796695+seankzw@users.noreply.github.com>
2023-10-30 12:49:23 +0800

    kalman filter (not tested)

--- ultrasonic_sensor/ultrasonic_sensor.c
+++ ultrasonic_sensor/ultrasonic_sensor.c
@@ -26,6 +26,7 @@ float getPulse(uint trigPin, uint echoPin)
     gpio_put(trigPin, 1);
     sleep_us(10);
     gpio_put(trigPin, 0);
+    sleep_us(2);
 
     while (gpio_get(echoPin) == 0)
     {
@@ -41,6 +42,7 @@ float getPulse(uint trigPin, uint echoPin)
         time2 = time_us_64();
         sleep_us(100);
     }
+
     float pulseLength = time2 - time1;
     return pulseLength;
 }
@@ -51,11 +53,24 @@ float getCm(uint trigPin, uint echoPin)
     float speedOfSound = 343.0;
 
     // Convert pulse length to distance in centimeters
-    float distanceCm = (pulseLength * speedOfSound) / 20000.0; // Dividing by 20000 to convert from microseconds to seconds
-
+    //float distanceCm = (pulseLength * speedOfSound) / 20000.0; // Dividing by 20000 to convert from microseconds to seconds
+    float distanceCm = (pulseLength * .034)/2;
     return distanceCm;
     //return (float)pulseLength / 2 * 0.0343;
     //return ((float)pulseLength * 0.0343) / 2;
+}
+
+float kalman(float U){
+  static const float R = 40;
+  static const float H = 1.00;
+  static float Q = 10;
+  static float P = 0;
+  static float U_hat = 0;
+  static float K = 0;
+  K = P*H/(H*P*H+R);
+  U_hat += + K*(U-H*U_hat);
+  P = (1-K*H)*P+Q;
+  return U_hat;
 }
 
 int main()
@@ -70,7 +85,8 @@ int main()
 
     while (1)
     {
-        printf("Object is about %.2f cm away", getCm(trigPin, echoPin));
+        printf("Object is about %.2f cm away (Original) \n", getCm(trigPin, echoPin));
+        printf("Object is about %.2f cm away (Kalman)\n", kalman(getCm(trigPin, echoPin)));
         sleep_ms(500);
     }
 }
